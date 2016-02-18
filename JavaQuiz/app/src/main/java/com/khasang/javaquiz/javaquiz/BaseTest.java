336fc44ddbdd6b3c2211b131be2cdf885453ba8e
Andrey Atomov <turunana@gmail.com>
2016-02-18 18:10:52 +0300

    add classes: BaseTest, PresenterImpl, enum TypeTest, interface IPresenter.

---
+++ JavaQuiz/app/src/main/java/com/khasang/javaquiz/javaquiz/BaseTest.java
@@ -0,0 +1,8 @@
+package com.khasang.javaquiz.javaquiz;
+
+/**
+ * Created by Andy on 18.02.2016.
+ */
+public class BaseTest {
+    //scatch
+}
---
+++ JavaQuiz/app/src/main/java/com/khasang/javaquiz/javaquiz/IPresenter.java
@@ -0,0 +1,9 @@
+package com.khasang.javaquiz.javaquiz;
+
+/**
+ * Created by Andy on 18.02.2016.
+ */
+public interface IPresenter {
+    public void unClick();
+    public BaseTest getTest(TypeTest typeTest);
+}
