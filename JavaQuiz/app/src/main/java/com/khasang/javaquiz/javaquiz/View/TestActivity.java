067ed15d0b978897dca50f72b4468e0dc3ed033e
xym4er <xym4er@gmail.com>
2016-02-26 15:24:03 +0300

    Допилил черновой вариант MainActivity и TestActivity

---
+++ JavaQuiz/app/src/main/java/com/khasang/javaquiz/javaquiz/View/TestActivity.java
@@ -0,0 +1,74 @@
+package com.khasang.javaquiz.javaquiz.View;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.khasang.javaquiz.javaquiz.R;
+import com.khasang.javaquiz.javaquiz.View.adapters.TabsPagerFragmentAdapter;
+import com.khasang.javaquiz.javaquiz.View.adapters.TestTabsPagerFragmentAdapter;
+
+public class TestActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private static final int LAYOUT = R.layout.activity_test;
+    private ViewPager viewPager;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+
+        initToolbar();
+        initTabs();
+        initFab();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initFab() {
+        fab = (FloatingActionButton)findViewById(R.id.fab_test);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_test);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu_main);
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager)findViewById(R.id.viewpager_test);
+        TestTabsPagerFragmentAdapter adapter = new TestTabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout_test);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
