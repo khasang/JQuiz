b9acbdde4429b152852c96b1f3a45a04f838e34d
Andrey Atomov <turunana@gmail.com>
2016-03-06 16:57:46 +0300

    rebase fragment_quick_test layout, add method choose count of questions.

--- JavaQuiz/app/src/main/java/com/khasang/javaquiz/javaquiz/View/fragments/QuickTestFragment.java
+++ JavaQuiz/app/src/main/java/com/khasang/javaquiz/javaquiz/View/fragments/QuickTestFragment.java
@@ -3,17 +3,25 @@ package com.khasang.javaquiz.javaquiz.View.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.khasang.javaquiz.javaquiz.R;
 
 public class QuickTestFragment extends Fragment {
     private static final int FRAGMENT_QUICK_TEST = R.layout.fragment_quick_test;
     private View view;
+
+    private SeekBar countSeekBar;
+    private TextView countTextView;
+
+    private int questionCount;
+    final int QUESTION_MIN = 1;
+    final int QUESTION_MAX = 100;
+    final int QUESTION_DEFAULT = 10;
 
     public static QuickTestFragment getInstance(){
         Bundle args = new Bundle();
@@ -27,7 +35,34 @@ public class QuickTestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(FRAGMENT_QUICK_TEST, container, false);
 
+        initSeekBar();
+
         return view;
     }
 
+    public void initSeekBar(){
+        countSeekBar = (SeekBar) view.findViewById(R.id.seekBarQuickTest);
+        countTextView = (TextView) view.findViewById(R.id.textViewQuickTestCount);
+        countSeekBar.setMax(QUESTION_MAX - QUESTION_MIN);
+
+        countSeekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        questionCount = QUESTION_MIN + progress;
+                        countTextView.setText(getResources().getString(R.string.settings_label_count) + Integer.toString(questionCount));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        countTextView.setText(getResources().getString(R.string.settings_label_count) + Integer.toString(questionCount));
+                    }
+                }
+        );
+    }
 }
